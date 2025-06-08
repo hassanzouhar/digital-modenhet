f3a3652ce3e8cad9c9b427f9740685588bf1e398
Hassan Z <43142217+hassanzouhar@users.noreply.github.com>
2025-06-08 11:49:05 +0200

    Add OG image generator

---
+++ api/og.js
@@ -0,0 +1,30 @@
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default function handler(req) {
+  const { searchParams } = new URL(req.url);
+  const title = searchParams.get('title') || 'Digital Modenhet';
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 64,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {title}
+      </div>
+    ),
+    { width: 1200, height: 630 }
+  );
+}
